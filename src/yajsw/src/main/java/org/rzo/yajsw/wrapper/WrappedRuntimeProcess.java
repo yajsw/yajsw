ddc54cc8d2b461872a107a9008cd6e60aef91ba8
rzorzorzo <rzorzorzo@users.sourceforge.net>
2013-11-03 11:11:33 +0100

    Bug: Runtime process does not restart when invoked through a trigger

--- src/yajsw/src/main/java/org/rzo/yajsw/wrapper/WrappedRuntimeProcess.java
+++ src/yajsw/src/main/java/org/rzo/yajsw/wrapper/WrappedRuntimeProcess.java
@@ -25,24 +25,26 @@ public class WrappedRuntimeProcess extends AbstractWrappedProcess
 		}
 		// TODO check if c exists - search in PATH
 		command.add(c);
-		for (Iterator it = _config.getKeys("wrapper.app.parameter"); it.hasNext();)
+		for (Iterator it = _config.getKeys("wrapper.app.parameter"); it
+				.hasNext();)
 		{
-			String p = _config.getString((String)it.next());
+			String p = _config.getString((String) it.next());
 			if (p != null)
 			{
 				p = p.trim();
 				if (p.length() > 0)
 					command.add(p);
-			}	
+			}
 		}
 		String[] arrCmd = new String[command.size()];
 		for (int i = 0; i < arrCmd.length; i++)
 			arrCmd[i] = (String) command.get(i);
 
 		_osProcess.setCommand(arrCmd);
-		//_osProcess.setPipeStreams(true, false);
+		// _osProcess.setPipeStreams(true, false);
 		// set this to false at your own risk.
-		boolean pipeStreams = _config.getBoolean("wrapper.console.pipestreams",true);
+		boolean pipeStreams = _config.getBoolean("wrapper.console.pipestreams",
+				true);
 		_osProcess.setPipeStreams(pipeStreams, pipeStreams);
 	}
 
@@ -62,18 +64,21 @@ public class WrappedRuntimeProcess extends AbstractWrappedProcess
 	{
 		try
 		{
-			_osProcess = OperatingSystem.instance().processManagerInstance().getProcess(pid);
+			_osProcess = OperatingSystem.instance().processManagerInstance()
+					.getProcess(pid);
 			if (!_osProcess.stop(10, 0))
 			{
-				getWrapperLogger().severe("native processes must be restarted to consume the out and err streams. stopping of process failed");
+				getWrapperLogger()
+						.severe("native processes must be restarted to consume the out and err streams. stopping of process failed");
 			}
 
 			// this.start();
 		}
 		catch (Throwable ex)
 		{
-			getWrapperLogger().severe(
-					"native processes must be restarted to consume the out and err streams. stopping of process failed: " + ex.getMessage());
+			getWrapperLogger()
+					.severe("native processes must be restarted to consume the out and err streams. stopping of process failed: "
+							+ ex.getMessage());
 			ex.printStackTrace();
 		}
 		return true;
@@ -89,34 +94,38 @@ public class WrappedRuntimeProcess extends AbstractWrappedProcess
 		}
 	}
 
-	private final ControllerListener	listenerStopped	= new ControllerListener()
-														{
-															public void fire()
-															{
-																getWrapperLogger().info("listener stopped");
-																if (_osProcess.isRunning())
-																	stop();
-																if (allowRestart() && exitCodeRestart() && !exitCodeShutdown())
-																{
-																	restartInternal();
-																}
-																else
-																{
-																	setState(STATE_IDLE);
-																	if (_debug)
-																	{
-																		getWrapperLogger().info("giving up after " + _restartCount + " retries");
-																	}
-																}
+	private final ControllerListener listenerStopped = new ControllerListener()
+	{
+		public void fire()
+		{
+			if (_state == STATE_RESTART_STOP || _state == STATE_RESTART || _state == STATE_RESTART_WAIT)
+				return;
+			getWrapperLogger().info("listener stopped");
+			if (_osProcess.isRunning())
+				stop();
+			if (allowRestart() && exitCodeRestart() && !exitCodeShutdown())
+			{
+				restartInternal();
+			}
+			else
+			{
+				setState(STATE_IDLE);
+				if (_debug)
+				{
+					getWrapperLogger().info(
+							"giving up after " + _restartCount + " retries");
+				}
+			}
 
-															}
+		}
 
-														};
+	};
 
 	void configController()
 	{
 		_controller.setLogger(getWrapperLogger());
-		_controller.addListener(RuntimeController.STATE_STOPPED, listenerStopped);
+		_controller.addListener(RuntimeController.STATE_STOPPED,
+				listenerStopped);
 
 	}
 
