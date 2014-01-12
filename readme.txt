be53f94f61de35b19560bbca0eea29714ff185e4
rzorzorzo <rzorzorzo@users.sourceforge.net>
2014-01-12 12:05:04 +0100

    release 11.11

--- readme.txt
+++ readme.txt
@@ -1,3 +1,5 @@
-yajsw-stable-11.10 
+yajsw-stable-11.11
 
-    * Bug: Problems with quotes in windows services and if classpath path contains blank
+    Bug: on application restart: stream of prev process is open although new process started.
+    Bug: 11.10 zip file incorrect version
+    New: new: Property wrapper.ntservice.process_priority
--- src/yajsw/src/main/java/org/rzo/yajsw/YajswVersion.java
+++ src/yajsw/src/main/java/org/rzo/yajsw/YajswVersion.java
@@ -2,7 +2,7 @@ package org.rzo.yajsw;
 
 public class YajswVersion
 {
-	public static final String YAJSW_VERSION = "yajsw-stable-11.09";
+	public static final String YAJSW_VERSION = "yajsw-stable-11.11";
 	public static final String OS_VERSION = System.getProperty("os.name")+"/"+System.getProperty("os.version")+"/"+System.getProperty("os.arch");
 	public static final String JAVA_VERSION = System.getProperty("java.vendor")+"/"+System.getProperty("java.version")+"/"+System.getProperty("java.home")+"/"+(System.getProperty("sun.arch.data.model") != null ? System.getProperty("sun.arch.data.model"):"unknown");
 }
